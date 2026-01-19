36299baf948764507db2d77b55c0b72a8e02d7c3
Jee <jee.vim@proton.me>
2026-01-19 10:24:22 +0700

    chore: update nginx host

--- config/nginx/host.nix
+++ config/nginx/host.nix
@@ -7,10 +7,10 @@
       root = "/var/www/portfolio";
     };
 
-    # "blog.example.com" = {
-    #   enableACME = true;
-    #   forceSSL = true;
-    #   root = "/var/www/blog";
+    # "portfolio" = {
+    #   # enableACME = true;
+    #   # forceSSL = true;
+    #   root = "/var/www/portfolio";
     # };
   };
 }
