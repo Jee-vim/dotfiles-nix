001cc9ed5642598b79ce6ba5512793f43138a7ee
Jee <jee.vim@proton.me>
2026-05-18 07:46:14 +0700

    feat: pi config (agents,settings)

--- home/terminal/pi/default.nix
+++ home/terminal/pi/default.nix
@@ -4,6 +4,12 @@ in {
   xdg.configFile."../.pi/agent/AGENTS.md".source =
     config.lib.file.mkOutOfStoreSymlink "${configSourceDir}/AGENTS.md";
 
+  xdg.configFile."../.pi/agent/settings.json".source =
+    config.lib.file.mkOutOfStoreSymlink "${configSourceDir}/settings.json";
+
   xdg.configFile."../.pi/agent/skills".source =
     config.lib.file.mkOutOfStoreSymlink "${configSourceDir}/skills";
+
+  xdg.configFile."../.pi/agent/agents".source =
+    config.lib.file.mkOutOfStoreSymlink "${configSourceDir}/agents";
 }
