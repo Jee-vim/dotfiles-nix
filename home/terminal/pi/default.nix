41b935327e89812a9cad3c59068d6eb699394806
Jee <jee.vim@proton.me>
2026-05-19 07:56:41 +0700

    wip: config pi

--- home/terminal/pi/default.nix
+++ home/terminal/pi/default.nix
@@ -1,15 +1,15 @@
 {config, ...}: let
   configSourceDir = toString ./.;
 in {
-  xdg.configFile."../.pi/agent/AGENTS.md".source =
+  home.file.".pi/agent/AGENTS.md".source =
     config.lib.file.mkOutOfStoreSymlink "${configSourceDir}/AGENTS.md";
 
-  xdg.configFile."../.pi/agent/settings.json".source =
+  home.file.".pi/agent/settings.json".source =
     config.lib.file.mkOutOfStoreSymlink "${configSourceDir}/settings.json";
 
-  xdg.configFile."../.pi/agent/skills".source =
+  home.file.".pi/agent/skills".source =
     config.lib.file.mkOutOfStoreSymlink "${configSourceDir}/skills";
 
-  xdg.configFile."../.pi/agent/agents".source =
+  home.file.".pi/agent/agents".source =
     config.lib.file.mkOutOfStoreSymlink "${configSourceDir}/agents";
 }
